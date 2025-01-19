6c7ed4277037a4b6f96fedc56eb9c2cd8e6208c0
TawanaH <tkghons@gmail.com>
2025-01-19 01:41:42 -0500

    Update: renaming some files

--- backend/ai/js/generateSchedules.js
+++ backend/ai/js/generateSchedules.js
@@ -1,11 +1,9 @@
 import { generateSchedulesFromInput } from "./scheduleProcessor.js";
 
-async function test() {
+async function generateSchedules() {
   try {
-    // You can test either with raw text or an audio file.
     const result = await generateSchedulesFromInput({
     //   rawText: "I want to mandatory register COMP1405, COMP1805, MATH1007, BIOL1902, PSYC1001"
-      // Or use an audio file by providing:
       audioFilePath: "test3.m4a"
     });
     console.log("Final Schedules JSON:\n", JSON.stringify(result, null, 2));
@@ -14,4 +12,4 @@ async function test() {
   }
 }
 
-test();
+generateSchedules();
